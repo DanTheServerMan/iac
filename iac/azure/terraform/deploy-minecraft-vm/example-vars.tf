87c4ebe5bae5ddf11d249e4988a760a5e892214b
DanTheServerMan <danielmthomas456@gmail.com>
2024-11-24 20:44:37 -0500

    Further testing and fixing

--- iac/azure/terraform/deploy-minecraft-vm/example-vars.tf
+++ iac/azure/terraform/deploy-minecraft-vm/example-vars.tf
@@ -1,10 +1,10 @@
  # You should fill out this file accordingly to your use case and also rename it to just "vars.tf"
-  variable "whitelist_ip" {
-    type = string
-    default = "YOUR_PUBLIC_IP"
-  }
+ # variable "whitelist_ip" {
+ #   type = string
+ #   default = "YOUR_PUBLIC_IP"
+ # }
 
-  variable "username" {
-    type = string
-    default = "YOUR_USERNAME"
-  }
+ # variable "username" {
+ #   type = string
+ #   default = "YOUR_USERNAME"
+ # }
